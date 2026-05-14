ef9f7d46ab839d1654edd5fc677bd9409210afc5
longpham-lop <tuanlongp70l@example.com>
2026-05-14 15:27:06 +0700

    deploy

--- src/indexs.ts
+++ src/indexs.ts
@@ -87,7 +87,7 @@ const init = async () => {
 init();
 
 /* ================= START SERVER ================= */
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
